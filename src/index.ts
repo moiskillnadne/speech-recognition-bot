62dc853c8944fc238ef948b15516f609d0a5a037
Victor Ryabkov <victorryabkov@MacBook-Pro-Victor.local>
2021-09-04 21:32:25 +0300

    feature: lint / prettier

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,75 @@
-require('dotenv').config()
-
+import dotenv from 'dotenv'
 import { Telegraf } from 'telegraf'
 import speech from '@google-cloud/speech'
 import CloudConvert from 'cloudconvert'
 import axios from 'axios'
 
+dotenv.config()
 
-const getFileAPI = (fileId: string) => `https://api.telegram.org/bot${process.env.telegramBotToken}/getFile?file_id=${fileId}`
+const { telegramBotToken, cloudConverterToken } = process.env
 
+const getFileAPI = (fileId: string) =>
+  `https://api.telegram.org/bot${telegramBotToken}/getFile?file_id=${fileId}`
 
 const bot = new Telegraf(process.env.telegramBotToken)
 const client = new speech.SpeechClient()
-const cloudConverter = new CloudConvert('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNTQ5ODZkMDVkMWY5MThlMTA4NDk1Mzk0YWE2NWIyZWYyZDc2MzQxMTY4NWFlNjRiNDZiN2ZhOTMwYjlkYzJhMzAyMzI1ZDk0MTcyNzM5ZTgiLCJpYXQiOjE2MzA3NTE1MTYuMjc4ODUsIm5iZiI6MTYzMDc1MTUxNi4yNzg4NTMsImV4cCI6NDc4NjQyNTExNi4yNDUyNDgsInN1YiI6IjUzMjE3ODk4Iiwic2NvcGVzIjpbInVzZXIucmVhZCIsInVzZXIud3JpdGUiLCJ0YXNrLnJlYWQiLCJ0YXNrLndyaXRlIiwid2ViaG9vay5yZWFkIiwid2ViaG9vay53cml0ZSIsInByZXNldC5yZWFkIiwicHJlc2V0LndyaXRlIl19.GW_3x1JXDqQsoK2UkVH8nI0GeL1JPpC2CvUqNimiHXp2WKGfjRoITT_lk7uI_4kcK7l1cd0o-Z8MB0wmy81X36AMg424gHbodVbQ_D_t8JKDQWB_sjySGOUC1_SHFK7UgLD4zR3aP1ha3ovmBLt9i5qcqNBvSRM2LAqxcistY5J2Cspjwnog-XHj2y5FTl9wg4SVO2HYr4GKcDZxGMtxfYf9P0KqZCztHwKbYZmLnpaquniQ78D2kV4GcdpMkfE6T7dGAh5A_CMvoSbshr_9Prw5zLthpDKR0MiSvrWRZy83f3gXN_B-8-2BxsJ0qpc6q6d4GBdU23rSed3SBn0rZKO49z2pGa4J1g4rH1lho2hdgkQ6IXVWbj6MYUTNOi2O6f-SWKDxn1YyM-OVgBSTkvd2BqQAx3hnVFotGwt8UEZ8UbcMjo9vFKxF0lSR84K9A3P_f8ZvExcF2k35bFQae6cwNk5w5RORX7nHfCH9FSslq7QYqIHCToVRhLhd1nbN0vxR0GO1RbNYyJ_43qjICZddsP_8RBrQfjZ4MQM29EFj-HZbvvapBDsu9SKgX_3--Zi-dDkv5_0y50M3Ursa9X1T4dvUGug29LfAtn-1KFRgbhqUNiW1y7ZK2YpZumoIEqHHIHl4sJau3RWxp9BgxKYO42FvzvPntSRH_yK0Of8')
+const cloudConverter = new CloudConvert(cloudConverterToken)
 
 bot.start((ctx) => ctx.reply('Welcome!'))
 
 bot.on('voice', async (ctx) => {
-    const linkToFile = getFileAPI(ctx.update.message.voice.file_id)
-    const { data } = await axios(linkToFile)
-    const filePath = `https://api.telegram.org/file/bot${process.env.telegramBotToken}/${data.result.file_path}`
+  const linkToFile = getFileAPI(ctx.update.message.voice.file_id)
+  const { data } = await axios(linkToFile)
+  const filePath = `https://api.telegram.org/file/bot${telegramBotToken}/${data.result.file_path}`
 
-    const job = await cloudConverter.jobs.create({
-        tasks: {
-            'import-my-file': {
-                operation: 'import/url',
-                url: filePath
-            },
-            'convert-my-file': {
-                operation: 'convert',
-                input: 'import-my-file',
-                output_format: 'flac',
-            },
-            'export-my-file': {
-                operation: 'export/url',
-                input: 'convert-my-file'
-            }
-        }
-    })
+  const job = await cloudConverter.jobs.create({
+    tasks: {
+      'import-my-file': {
+        operation: 'import/url',
+        url: filePath,
+      },
+      'convert-my-file': {
+        operation: 'convert',
+        input: 'import-my-file',
+        output_format: 'flac',
+      },
+      'export-my-file': {
+        operation: 'export/url',
+        input: 'convert-my-file',
+      },
+    },
+  })
 
-    const jobResult = await cloudConverter.jobs.wait(job.id)
-    const exportTask = jobResult.tasks.filter(
-        task => task.operation === 'export/url' && task.status === 'finished'
-    )[0];
-    const file = exportTask.result.files[0];
+  const jobResult = await cloudConverter.jobs.wait(job.id)
+  const exportTask = jobResult.tasks.filter(
+    (task) => task.operation === 'export/url' && task.status === 'finished'
+  )[0]
+  const file = exportTask.result.files[0]
 
-    console.log(file)
-    // Google config request
-    const request = {
-        audio: {
-            content: file.url
-        },
-        config: {
-            sampleRateHertz: 48000,
-            audioChannelCount: 1,
-            languageCode: 'ru-RU'
-        },
-    };
+  console.log(file)
+  // Google config request
+  const request = {
+    audio: {
+      content: file.url,
+    },
+    config: {
+      sampleRateHertz: 48000,
+      audioChannelCount: 1,
+      languageCode: 'ru-RU',
+    },
+  }
 
-    try {
-        console.log(request)
-        const [response] = await client.recognize(request);
-        const transcription = response.results
-            .map(result => result.alternatives[0].transcript)
-            .join('\n');
-        console.log(`Transcription: ${transcription}`);
-    } catch (err) {
-        console.log('Error')
-        console.error(err)
-    }
+  try {
+    console.log(request)
+    const [response] = await client.recognize(request)
+    const transcription = response.results
+      .map((result) => result.alternatives[0].transcript)
+      .join('\n')
+    console.log(`Transcription: ${transcription}`)
+  } catch (err) {
+    console.log('Error')
+    console.error(err)
+  }
 })
 
 // bot.on('voice', async (ctx) => {
@@ -104,12 +106,11 @@ bot.on('voice', async (ctx) => {
 // })
 
 bot.on('text', (ctx) => {
-    console.log(ctx)
+  console.log(ctx)
 })
 
 bot.on('document', (ctx) => {
-    console.log(ctx.update)
+  console.log(ctx.update)
 })
-
 
 bot.launch()
